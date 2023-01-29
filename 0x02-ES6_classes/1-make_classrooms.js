edc3c03e893ef1e19957f6ab2260ebebcca12b6f
Dennisco12 <akinwonjowodennisco@gmail.com>
2023-01-29 06:27:04 -0800

    Added task 1

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,7 +1,7 @@
 import ClassRoom from './0-classroom';
 
-export default function initializeRooms () {
-  let array = []
+export default function initializeRooms() {
+  const array = [];
   array.push(ClassRoom(19));
   array.push(ClassRoom(20));
   array.push(ClassRoom(34));
