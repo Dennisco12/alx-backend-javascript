2efc4781f157f1ca65c9ba40cc3019d8e9528dce
Dennisco12 <akinwonjowodennisco@gmail.com>
2023-03-25 10:03:44 +0000

    Added solutions to the nodejs basic project

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,38 @@
+const fs = require('fs')
+
+function countStudents(path) {
+  try {
+    students = {}
+    length = 0
+    fields = {}
+    const content = fs.readFileSync(path, 'utf-8');
+    const lines = content.toString().split('\n');
+    for (let i = 0; i < lines.length; i += 1) {
+      if (lines[i]) {
+        length += 1;
+        const field = lines[i].toString().split(',')
+        if (Object.prototype.hasOwnProperty.call(students, field[3])) {
+          students[field[3]].push(field[0]);
+        } else {
+          students[field[3]] = [field[0]];
+        }
+        if (Object.prototype.hasOwnProperty.call(fields, field[3])) {
+          fields[field[3]] += 1;
+        } else {
+          fields[field[3]] = 1;
+        }
+      }
+    }
+    const l = length - 1;
+    console.log(`Number of students: ${l}`);
+    for (const [key, val] of Object.entries(fields)) {
+      if (key !== 'field') {
+        console.log(`Number of students in ${key}: ${val}. List: ${students[key].join(', ')}`);
+      }
+    }
+  } catch (error) {
+    throw new Error('Cannot load the database');
+  }
+}
+
+module.exports = countStudents;
---
+++ 0x05-Node_JS_basic/README.md
@@ -0,0 +1,1 @@
+This is a soution to all the tasks in the Node-js basic project
